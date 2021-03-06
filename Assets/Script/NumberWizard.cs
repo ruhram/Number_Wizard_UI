ee3c013d73a37b9de4351d910a19425ed2748c91
LAPTOP-EAT55AGK\Oscarium <rullyrgandadiwirya@gmail.com>
2021-03-06 15:46:59 +0700

    gameplay of the game

---
+++ Assets/Script/NumberWizard.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField]int max;
+    [SerializeField]int min;
+    [SerializeField] TextMeshProUGUI guessText;
+    int guess;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        guess = (max + min) / 2;
+        guessText.text = guess.ToString();
+        max = max + 1;
+    }
+
+    public void onPressHigher()
+    {
+        min = guess;
+        NextGuess();
+    }
+    public void onPressLower()
+    {
+        max = guess;
+        NextGuess();
+    }
+    public void NextGuess()
+    {
+        guess = (min + max) / 2;
+        guessText.text = guess.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
